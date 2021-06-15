6972aa09e643c1202ef51397b8a35b62e8bb2be1
Your Name <you@example.com>
2021-06-15 07:37:07 +0000

    Fix

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,16 +1,59 @@
-import React from 'react';
-import { Paragraph } from '@contentful/forma-36-react-components';
+import React, { useEffect, useState } from 'react'
+import { TextInput } from '@contentful/forma-36-react-components';
 import { FieldExtensionSDK } from '@contentful/app-sdk';
+
 
 interface FieldProps {
   sdk: FieldExtensionSDK;
 }
 
 const Field = (props: FieldProps) => {
-  // If you only want to extend Contentful's default editing experience
-  // reuse Contentful's editor components
-  // -> https://www.contentful.com/developers/docs/extensibility/field-editors/
-  return <Paragraph>Hello Entry Field Component</Paragraph>;
+
+  const [value, setValue] = useState(props.sdk.field.getValue() || []);
+  const [publishCount, setPublishCount] = useState(props.sdk.entry.getSys().publishedCounter);
+
+  const handleObjectUpdate = ((sys: any) => {
+    if (sys) {
+      setPublishCount(sys.publishedCounter);
+    }
+  });
+
+  useEffect(() => {
+    props.sdk.window.startAutoResizer(); // use contentful's builtin auto-resizer
+    props.sdk.entry.onSysChanged(handleObjectUpdate); // update publishCount
+  });
+
+  // external change
+  useEffect(() => {
+    const detachExternalChangeHandler = () => props.sdk.field.onValueChanged(setValue)
+  });
+
+  const onChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.currentTarget.value;
+    // console.log(value);
+    setValue( value );
+    if (value) {
+      await props.sdk.field.setValue(value)
+          .then((data) => {
+            // console.log(data);  // do something else
+          });
+    } else {
+      await props.sdk.field.removeValue();
+    }
+  }
+
+  return (
+      <TextInput
+          width="large"
+          type="text"
+          id="my-field"
+          testId="my-field"
+          value={value}
+          onChange={onChange}
+          readOnly={publishCount > 0 ? true : false}
+      />
+  );
+
 };
 
 export default Field;
